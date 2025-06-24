687a2b550c7061793a743efac2e94e2b2ac7c246
Chetana-codes <budgetchetana@gmail.com>
2025-06-24 10:21:23 +0530

    Create 11_substring.py

---
+++ 11_substring.py
@@ -0,0 +1,7 @@
+text = input("Enter a string: ")
+sub = input("Enter a substring: ")
+
+if sub in text:
+    print("Yes, the substring is in the string.")
+else:
+    print("No, the substring is not in the string.")
